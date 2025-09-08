80483f461c881cdd498e3787884781c56b7e35fc
BurakYildrm <yldrmburak1167@gmail.com>
2025-09-08 18:54:26 +0300

    Updated skeleton size for mobile

--- src/app/(private)/loading.tsx
+++ src/app/(private)/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <Skeleton className="size-60 md:size-70 lg:size-94 rounded-full" />
-      <Skeleton className="h-4 w-100" />
+      <Skeleton className="h-4 w-60 md:w-100" />
     </div>
   );
 }
